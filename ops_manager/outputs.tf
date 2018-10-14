cf75c0d49441d93a1a67b3e326b5a7b8dbc35b9e
MohamedAltaf <basheer.altaf@gmail.com>
2018-10-14 17:15:55 -0500

    Removed DNS Section

--- ops_manager/outputs.tf
+++ ops_manager/outputs.tf
@@ -6,14 +6,6 @@ output "public_ip" {
   value = "${element(concat(aws_eip.ops_manager.*.public_ip, list("")), 0)}"
 }
 
-output "dns" {
-  value = "${element(concat(aws_route53_record.ops_manager.*.name, list("")), 0)}"
-}
-
-output "optional_dns" {
-  value = "${element(concat(aws_route53_record.optional_ops_manager.*.name, list("")), 0)}"
-}
-
 output "security_group_id" {
   value = "${element(concat(aws_security_group.ops_manager_security_group.*.id, list("")), 0)}"
 }
