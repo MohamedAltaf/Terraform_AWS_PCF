f100d7b9baf3e685e15f40d2c884770935757cd7
MohamedAltaf <basheer.altaf@gmail.com>
2018-10-16 16:15:11 -0500

    Included availability_zone for all the 9 subnets

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "management_subnet_1" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.management_cidr_1}"
+  availability_zone = "${element(var.availability_zones, 0)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-management-subnet-1")
@@ -10,6 +11,7 @@ resource "aws_subnet" "management_subnet_1" {
 resource "aws_subnet" "management_subnet_2" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.management_cidr_2}"
+  availability_zone = "${element(var.availability_zones, 1)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-management-subnet-2")
@@ -19,6 +21,7 @@ resource "aws_subnet" "management_subnet_2" {
 resource "aws_subnet" "management_subnet_3" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.management_cidr_3}"
+  availability_zone = "${element(var.availability_zones, 2)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-management-subnet-3")
@@ -28,6 +31,7 @@ resource "aws_subnet" "management_subnet_3" {
 resource "aws_subnet" "pas_subnet_1" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.pas_cidr_1}"
+  availability_zone = "${element(var.availability_zones, 0)}"
 
   tags {
     Name = "${var.env_name}-ert-subnet-1}"
@@ -37,6 +41,7 @@ resource "aws_subnet" "pas_subnet_1" {
 resource "aws_subnet" "pas_subnet_2" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.pas_cidr_2}"
+  availability_zone = "${element(var.availability_zones, 1)}"
 
   tags {
     Name = "${var.env_name}-ert-subnet-2}"
@@ -46,6 +51,7 @@ resource "aws_subnet" "pas_subnet_2" {
 resource "aws_subnet" "pas_subnet_3" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.pas_cidr_3}"
+  availability_zone = "${element(var.availability_zones, 2)}"
 
   tags {
     Name = "${var.env_name}-ert-subnet-3}"
@@ -55,6 +61,7 @@ resource "aws_subnet" "pas_subnet_3" {
 resource "aws_subnet" "services_subnet_1" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.services_cidr_1}"
+  availability_zone = "${element(var.availability_zones, 0)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-services-subnet-1")
@@ -64,6 +71,7 @@ resource "aws_subnet" "services_subnet_1" {
 resource "aws_subnet" "services_subnet_2" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.services_cidr_2}"
+  availability_zone = "${element(var.availability_zones, 1)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-services-subnet-2")
@@ -73,6 +81,7 @@ resource "aws_subnet" "services_subnet_2" {
 resource "aws_subnet" "services_subnet_3" {
   vpc_id            = "${aws_vpc.vpc.id}"
   cidr_block        = "${var.services_cidr_3}"
+  availability_zone = "${element(var.availability_zones, 2)}"
 
   tags = "${merge(var.tags, local.default_tags,
     map("Name", "${var.env_name}-services-subnet-3")
