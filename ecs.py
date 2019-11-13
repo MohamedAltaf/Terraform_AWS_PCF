c28058030419546b23d196d3f17af24fd9ebcbb2
MohamedAltaf <basheer.altaf@gmail.com>
2019-11-13 16:31:13 -0700

    updated ecs delete function

--- ecs.py
+++ ecs.py
@@ -3,73 +3,87 @@ import json
 # Create ECS client
 client = boto3.client("ecs", region_name="eu-west-1")
 
-# # ECS TaskSet Deletion
-# # Pass ARN's of Cluster, Service and TaskSet
-# def delete_task_set(region, taskset_arn, metadata, boto_session = None):
-#     try:
-#         client.delete_task_set(
-#             cluster=metadata["cluster"],
-#             service=metadata["service"],
-#             taskSet=taskset_arn
-#         )
-#         print("ECS TaskSet {} is successfully deleted".format(taskset_arn))
-#     except client.exceptions.TaskSetNotFoundException as e:
-#         print(e)
-#     except client.exceptions.ResourceNotFoundException as e:
-#         print(e)
-#
-# # ECS Service Deletion
-# # Pass Service ARN and Cluster ARN
-# def delete_service(region, service_arn, metadata, boto_session = None):
-#     try:
-#         client.delete_service(
-#             service=service_arn,
-#             cluster=metadata["cluster"]
-#         )
-#         print("ECS Service {} is successfully deleted".format(service_arn))
-#     except client.exceptions.ServiceNotFoundException as e:
-#         print(e)
-#     except client.exceptions.ResourceNotFoundException as e:
-#         print(e)
+# ECS TaskSet Deletion
+# Pass ARN's of Cluster, Service and TaskSet
+def delete_taskset(region, taskset_arn, metadata, boto_session = None):
+
+    try:
+        client.delete_task_set(
+            cluster=metadata["cluster"],
+            service=metadata["service"],
+            taskSet=taskset_arn
+        )
+        print("TaskSet {} is successfully deleted".format(taskset_arn))
+
+    except client.exceptions.TaskSetNotFoundException as e:
+        print(e)
+    except client.exceptions.ServiceNotActiveException as e:
+        print(e)
+    except client.exceptions.ServiceNotFoundException as e:
+        print(e)
+    except client.exceptions.ClusterNotFoundException as e:
+        print(e)
+
+# ECS Service Deletion
+# Pass Service ARN and Cluster ARN
+def delete_service(region, service_arn, metadata, boto_session = None):
+
+    try:
+        client.delete_service(
+            service=service_arn,
+            cluster=metadata["cluster"],
+            force=True
+        )
+        print("Service {} is successfully deleted".format(service_arn))
+
+    except client.exceptions.ServiceNotFoundException as e:
+        print(e)
+    except client.exceptions.ClusterNotFoundException as e:
+        print(e)
+
 
 # ECS Cluster Deletion
 # Pass the Cluster ARN
-
 def delete_cluster(region, cluster_arn, boto_session = None):
-    try:
-        response = client.list_container_instances(
-            cluster=cluster_arn
-        )
-        container_instances = response['containerInstanceArns']
-        if container_instances != []:
-            for container_instance_arn in response['containerInstanceArns']:
-                client.deregister_container_instance(
-                    cluster=cluster_arn,
-                    containerInstance=container_instance_arn
-                )
-            print('Container Instances are successfully deleted')
-    except client.exceptions.ClusterNotFoundException as e:
-        print(e)
+
     try:
         response = client.list_tasks(
             cluster=cluster_arn
         )
         tasks = response['taskArns']
-        if tasks != []:
+        if tasks:
             for task_arn in response['taskArns']:
                 client.stop_task(
                     cluster=cluster_arn,
                     task=task_arn
                 )
-            print('Tasks are stopped')
+            print('Running tasks are stopped')
+
     except client.exceptions.ClusterNotFoundException as e:
         print(e)
+
+    try:
+        response = client.list_container_instances(
+            cluster=cluster_arn
+        )
+        container_instances = response['containerInstanceArns']
+        if container_instances:
+            for container_instance_arn in response['containerInstanceArns']:
+                client.deregister_container_instance(
+                    cluster=cluster_arn,
+                    containerInstance=container_instance_arn
+                )
+            print('Container Instances are successfully deregistered')
+
+    except client.exceptions.ClusterNotFoundException as e:
+        print(e)
+
     try:
         response = client.list_services(
             cluster=cluster_arn
         )
         services = response['serviceArns']
-        if services != []:
+        if services:
             for service_arn in response['serviceArns']:
                 client.delete_service(
                     cluster=cluster_arn,
@@ -77,24 +91,39 @@ def delete_cluster(region, cluster_arn, boto_session = None):
                     force=True
                 )
             print ('Services are successfully deleted')
+
     except client.exceptions.ClusterNotFoundException as e:
         print(e)
+
+    response = client.list_task_definitions()
+    task_definitions = response['taskDefinitionArns']
+
+    if task_definitions:
+
+        for task_definition_arn in response['taskDefinitionArns']:
+            client.deregister_task_definition(
+                taskDefinition=task_definition_arn
+            )
+        print('Task Definitions are successfully deregistered')
+
     try:
         client.delete_cluster(
             cluster=cluster_arn
         )
-        print("ECS Cluster {} is successfully deleted".format(cluster_arn))
+        print("Cluster {} is successfully deleted".format(cluster_arn))
+
     except client.exceptions.ClusterNotFoundException as e:
         print(e)
 
-# metadata_ecs_taskset = {
-#     "cluster":"arn:aws:ecs:eu-west-1:853157869693:cluster/example-cluster",
-#     "service":"arn:aws:ecs:eu-west-1:853157869693:service/myapp"
-# }
-#
-# metadata_ecs_service = {
-#     "cluster":"arn:aws:ecs:eu-west-1:853157869693:cluster/example-cluster"
-# }
-#
-# delete_service("us-east-1", "arn:aws:ecs:eu-west-1:853157869693:service/myapp", metadata_ecs_service)
+metadata_ecs_taskset = {
+    "cluster":"arn:aws:ecs:eu-west-1:853157869693:cluster/example-cluster",
+    "service":"arn:aws:ecs:eu-west-1:853157869693:service/test-ecs-external-service"
+}
+
+metadata_ecs_service = {
+    "cluster":"arn:aws:ecs:eu-west-1:853157869693:cluster/example-cluster"
+}
+
+delete_taskset("eu-west-1", "arn:aws:ecs:eu-west-1:853157869693:task-set/example-cluster/test-ecs-external-service/ecs-svc/9223370463167764101", metadata_ecs_taskset)
+delete_service("eu-west-1", "arn:aws:ecs:eu-west-1:853157869693:service/test-ecs-service", metadata_ecs_service)
 delete_cluster("eu-west-1", "arn:aws:ecs:eu-west-1:853157869693:cluster/example-cluster")
